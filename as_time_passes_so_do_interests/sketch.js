57febcdf0bb7ef5d16f596f8418d2435a6411815
Natalie Bellido <35817942+NatalieBellido@users.noreply.github.com>
2018-04-05 12:23:09 -0700

    added duplicate folder for net art project

---
+++ as_time_passes_so_do_interests/sketch.js
@@ -0,0 +1,34 @@
+let x = -80;
+var gif;
+var gif2;
+var right;
+
+function setup() {
+  var right = true;
+  createCanvas(500, 500);
+  gif = loadGif('Pokeball_Rolling_Other_Way.gif');
+  gif2 = loadGif('Pokeball_Rolling.gif');
+}
+
+function draw() {
+  background(0);
+  if (mouseIsPressed) {
+    move();
+  }
+  if (right) {
+    image(gif, x, 420, gif.width / 2, gif.height / 2);
+  } else {
+    image(gif2, x, 420, gif.width / 2, gif.height / 2);
+  }
+  if (x == -81 || x == 580) {
+    right = !right;
+  }
+}
+
+function move() {
+  if (right) {
+    x++;
+  } else {
+    x--;
+  }
+}
