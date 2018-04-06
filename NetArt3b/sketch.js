90221d1c830e1e884e25ae84e510f3030e98dc74
Natalie Bellido <35817942+NatalieBellido@users.noreply.github.com>
2018-04-06 06:43:42 -0700

    comments now

--- NetArt3b/sketch.js
+++ NetArt3b/sketch.js
@@ -1,17 +1,17 @@
 let x = -80;
-var gif;
-var jpg;
-var right;
+let poke1;
+let poke2;
+let right;
 
 //Have a pokeball have your name and the other have your sister's name. Then have your sister's ball roll off screen while your ball stays!
 function preload() {
-  jpg = loadImage('Pokeball.png');
+  poke1 = loadImage('Pokeball.png');
 }
 
-// function setup() {
-//   createCanvas(500, 500);
-//   gif = loadGif('Pokeball_Rolling_Other_Way.gif');
-// }
+function setup() {
+  createCanvas(500, 500);
+  poke2 = loadGif('Pokeball_Rolling_Other_Way.gif');
+}
 //
 // function draw() {
 //   background(0);
@@ -19,7 +19,7 @@ function preload() {
 //     move();
 //   }
 //   if (right) {
-//     image(gif, x, 420, gif.width / 2, gif.height / 2);
+//     image(poke2, x, 420, gif.width / 2, gif.height / 2);
 //   }
 //   if (x == -81 || x == 580) {
 //     right = !right;
