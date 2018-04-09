91f2eda4baa6f9fcd324f6b57649eef211c050ee
Natalie Bellido <35817942+NatalieBellido@users.noreply.github.com>
2018-04-08 20:58:45 -0700

    edited net art 3b

--- NetArt3b/sketch.js
+++ NetArt3b/sketch.js
@@ -1,32 +1,36 @@
-let x = -80;
-let poke1;
-let poke2;
-let right;
+let x = 250;
+var gif;
+var poke;
+var right;
 
-//Have a pokeball have your name and the other have your sister's name. Then have your sister's ball roll off screen while your ball stays!
 function preload() {
-  poke1 = loadImage('Pokeball.png');
+  poke = loadImage('Pokeball.png');
 }
 
 function setup() {
+  var right = true;
   createCanvas(500, 500);
-  poke2 = loadGif('Pokeball_Rolling_Other_Way.gif');
+  gif = loadGif('Pokeball_Rolling_Other_Way.gif');
 }
-//
-// function draw() {
-//   background(0);
-//   if (mouseIsPressed) {
-//     move();
-//   }
-//   if (right) {
-//     image(poke2, x, 420, gif.width / 2, gif.height / 2);
-//   }
-//   if (x == -81 || x == 580) {
-//     right = !right;
-//   }
-//
-//   function move() {
-//     if (right) {
-//       x++;
-//     }
-//   }
+
+function draw() {
+  background(0);
+  image(poke, 150, 420, gif.width / 2, gif.height / 2);
+  if (mouseIsPressed) {
+    move();
+  }
+  if (right) {
+    image(gif, x, 420, gif.width / 2, gif.height / 2);
+  }
+  if (x == 250 || x == 580) {
+    right = !right;
+  }
+}
+
+function move() {
+  if (right) {
+    x++;
+  } else {
+    x--;
+  }
+}
